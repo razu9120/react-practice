6f83a0d7dc304dcd0a63b00068407706cb71c197
razu9120 <yuzu8810@icloud.com>
2024-11-01 13:29:52 +0900

    emoji-counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"];
+  const currentEmoji = emojis[count % emojis.length];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="emoji">{currentEmoji}</div>
+        <p>You clicked {count} times</p>
+        <p>count % emojis.length: {count % emojis.length}</p>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
       </header>
     </div>
   );
