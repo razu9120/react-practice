fb2c69c3c677a0fdafcdda2f73ee29339c62f383
razu9120 <yuzu8810@icloud.com>
2024-11-05 14:12:58 +0900

    shopping-list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const shoppingList = [
+    { id: 1, name: 'Reactシューズ 20XX限定バージョン' },
+    { id: 2, name: 'Reactソーダ React Tシャツ オレンジ味' },
+    { id: 3, name: 'React Tシャツ Mondayモデル' }
+  ];
+  const listItems = shoppingList.map(item => <li key={item.id}>{item.name}</li>);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ul>{listItems}</ul>
     </div>
   );
 }
