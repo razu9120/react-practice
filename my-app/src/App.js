d36b9953510af1df7dbbece46c42d6502d7cf847
razu9120 <yuzu8810@icloud.com>
2024-11-07 09:22:13 +0900

    practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Test1 from '../src/components/Test1';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('');
+  const [flg, setFlg] = useState(true);
+  const str = "タイトル";
+  const props = "props";
+  const animals = ["Lion", "Elephant", "Tiger", "Giraffe", "Zebra"];
+  const sum = (num1, num2) => {
+    return num1 + num2;
+  };
+  const nameCopy = (e) => {
+    return setName(e.target.value);
+  };
+  const changeFlg = () => {
+    return flg ? setFlg(false) : setFlg(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{str}</h1>
+      <ul>
+        {animals.map((animal, index) => <li key={index}>{animal}</li>)}
+      </ul>
+      <Test1 props={props} sum={sum} />
+      {/* <input type='text' id='input-name' name='input-name' onChange={(e) => setName(e.target.value)} />
+      <input type='text' id='output-name' name='output-name' value={name} /> */}
+      <input type='text' id='input-name' name='input-name' onChange={nameCopy} />
+      <input type='text' id='output-name' name='output-name' value={name} />
+      <button type='submit' onClick={changeFlg}>検索</button>
+      {flg ? <div>true</div> : <div>false</div>}
     </div>
   );
 }
